5110c71918d42a76f16581051450b8f77882991c
ITITIU20311 <ITITIU20311@student.hcmiu.edu.vn>
2021-10-26 00:10:26 +0700

    Update Exercise4.c

--- src/Exercise4.c
+++ src/Exercise4.c
@@ -49,8 +49,15 @@ void printArray(int a[SIZE][SIZE], int m, int n)
 
 void insertRow(int arr[], int a[SIZE][SIZE],int rowIndex, int m, int n){
 	//Your codes here
-	
-}
+	for(int i=m-1;i>=rowIndex+1;i=i-1){
+		for(int j=0;j<=n-1;j++){
+			a[i+1][j]=a[i][j];
+		}
+	}
+	for(int i=0;i<=n-1;i++){
+		arr[rowIndex][i]=arr[i];
+	}
+}	
 
 void removeRow(int a[SIZE][SIZE], int rowIndex, int m, int n){
 	//Your codes here
